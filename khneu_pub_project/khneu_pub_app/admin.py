ccd10e8abe603096b18134d48d6e1113fc4e8681
Danil Makhmudov <makhmudovdanil@gmail.com>
2023-11-06 22:50:26 +0100

    models migrations and registrations

--- khneu_pub_project/khneu_pub_app/admin.py
+++ khneu_pub_project/khneu_pub_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser,Specialization
+from .models import *
 
 admin.site.register(CustomUser)
+admin.site.register(Faculty)
 admin.site.register(Specialization)
+admin.site.register(Favorite)
+admin.site.register(Article)
+

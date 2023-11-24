aea45bef65b581e4be873ef5f7c7544aabea5af2
Danil Makhmudov <makhmudovdanil@gmail.com>
2023-11-24 12:36:38 +0100

    url tests

--- khneu_pub_project/khneu_pub_app/urls.py
+++ khneu_pub_project/khneu_pub_app/urls.py
@@ -2,27 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.custom_login, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('', views.home, name='home'),
     path('faculty/<slug:slug>', views.get_faculty, name='faculty'),
     path('specialization/<slug:slug>', views.get_specialization, name='specialization'),
     path('article/<slug:slug>', views.get_article, name='article'),
-    path('search/>', views.search, name='search'),
-    path('create_article/>', views.create_article, name='create_article'),
+    path('search/', views.search, name='search'),
+    path('create_article/', views.create_article, name='create_article'),
     path('article/<slug:article_slug>/add_to_favorite/', views.add_to_favorite, name='add_to_favorite'),
     path('update_article/<slug:slug>/',views.update_article, name='update_article'),
     path('delete_article/<slug:slug>/', views.delete_article, name='delete_article'),
     path('user_profile/<int:pk>/', views.get_user_profile, name='user_profile'),
     path('user_favorite_list/<int:user_pk>/', views.get_user_favorite_list, name='user_favorite_list'),
     path('user_articles_list/<int:user_pk>/', views.get_user_articles_list, name='user_articles_list'),
-
-
-
-#Later
     path('about/', views.get_about, name='about'),
     path('students/', views.get_students, name='students'),
     path('specs/', views.get_specs, name='specs'),
+    #Later
     path('contacts/', views.get_contacts, name='contacts'),
 ]
